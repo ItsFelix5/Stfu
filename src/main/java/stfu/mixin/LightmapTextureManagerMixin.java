a32ecfb89c86ca573e1d57541f0c156b6fe251d1
itsfelix5 <felixsoetewey@outlook.com>
2025-06-23 16:28:46 +0200

    Fix a bug with darkness, update YACL and fix my stupidity

--- src/main/java/stfu/mixin/LightmapTextureManagerMixin.java
+++ src/main/java/stfu/mixin/LightmapTextureManagerMixin.java
@@ -22,7 +22,6 @@ public class LightmapTextureManagerMixin {
     @Unique private long lastUpdate;
     @Unique private boolean hadConduitPower;
     @Unique private float lastSkyDarkness;
-    @Unique private boolean appliedDarkness;
     @Unique private double lastGamma;
 
     @SuppressWarnings("DataFlowIssue")
@@ -55,10 +54,7 @@ public class LightmapTextureManagerMixin {
         }
 
         // Darkness
-        if ((client.options.getDarknessEffectScale().getValue() > 0 && client.player.hasStatusEffect(StatusEffects.DARKNESS)) != appliedDarkness) {
-            appliedDarkness = !appliedDarkness;
-            return true;
-        }
+        if (client.player.hasStatusEffect(StatusEffects.DARKNESS) && client.options.getDarknessEffectScale().getValue() > 0) return true;
 
         // Sky Darkness (Boss Events)
         if(client.gameRenderer.skyDarkness != lastSkyDarkness){
